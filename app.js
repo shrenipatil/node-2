7a66c55abb1f636457263fc493dccdf875db3383
shrenipatil <shrenipatil@gmail.com>
2017-04-06 12:46:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/', function (req, res) {
 				console.log("temp:::::"+result);
 				console.log("pressure:::::"+result1);
 				res.end('Temperature:::::'+result+'c\nPressure::::'+result1+'\nHumidity::::'+result3);
-		    		/res.end('city::::'+city+'\nTemperature:::::'+result+'C');
+		    		//res.end('city::::'+city+'\nTemperature:::::'+result+'C');
 				//res.end('Temperature:::::'+result+'C');
              //   printWeather(weatherAPI.name, weatherAPI.main.temp);
             } catch(error) {
