be0e72d134d3f714a53d88f8d96c3bb481e233c1
shrenipatil <shrenipatil@gmail.com>
2017-04-06 14:09:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ var express = require('express');
 var app = express();
 var geoip = require('geoip-lite');
 app.set('port', (process.env.PORT || 5000))
-//Print message
-//Connect to the API URL api.openweathermap.org/data/2.5/weather?q={city name},{country code}
-//b0a4f9bb5260d1042b6aa08ab71caff8
-//var ip = "122.15.109.90";
 
+//Connect to the API URL api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+// key  ::  b0a4f9bb5260d1042b6aa08ab71caff8
 
 app.get('/', function (req, res) {
-		
-	//calculate city
+	//calculate IP to city
 	var ip=req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	console.log(ip);
 	var geo=geoip.lookup(ip);
@@ -21,7 +18,7 @@ app.get('/', function (req, res) {
 	
     var request = http.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=b0a4f9bb5260d1042b6aa08ab71caff8', function(response) {
     var body = '';
-
+	    
     //Read the data
     response.on('data', function(chunk) {
         body += chunk;
@@ -31,10 +28,8 @@ app.get('/', function (req, res) {
         if (response.statusCode === 200) {
             try {
                 //Parse the data
-                var weatherAPI = JSON.parse(body);
-
-                //Print the data
-				
+                		var weatherAPI = JSON.parse(body);
+           
 				var result = weatherAPI.main.temp-273.15;
 				console.log("temp:::::"+result);
 				var result1= weatherAPI.main.pressure;
@@ -43,9 +38,7 @@ app.get('/', function (req, res) {
 				console.log("pressure:::::"+result1);
 		    		console.log("humidity:::::"+result3);
 				res.end('Temperature:::::'+result+'c\nPressure::::'+result1+'mb\nHumidity::::'+result3+'%');
-		    		//res.end('city::::'+city+'\nTemperature:::::'+result+'C');
-				//res.end('Temperature:::::'+result+'C');
-             //   printWeather(weatherAPI.name, weatherAPI.main.temp);
+		  
             } catch(error) {
                 //Parse error
                 printError(error);
@@ -56,18 +49,11 @@ app.get('/', function (req, res) {
         }
     })
 });
-
 //Connection error
 request.on('error', function (err) {
-
 printError(err);
-
 });
-
-
 });
-
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
